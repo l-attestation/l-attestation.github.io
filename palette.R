69c399886a3ebe43ac3cc90c2858c7e2b6799013
tblk <theo.boulakia@orange.fr>
2023-09-27 22:39:05 +0200

    add email and anamosa link to about page

--- palette.R
+++ palette.R
@@ -1,6 +1,6 @@
 library(eyedroppeR)
 
-eyedropper(n = 8,
-           img_path = 'couverture.jpg')
+# eyedropper(n = 8,
+#            img_path = 'couverture.jpg')
 
 extract_pal(5, img_path = "couverture.jpg", label = "Caye Caulker, Belize", sort = "auto")
