3e914e294c88f2d6d7af7c852307c8625a20c073
tblk <theo.boulakia@orange.fr>
2024-02-20 09:22:49 +0100

    replace magrittr pipe with base pipe

--- _targets.R
+++ _targets.R
@@ -34,31 +34,31 @@ tar_plan(
   
   # Natural Earth ----------
   
-  world_sf = ne_countries(type = "countries", returnclass = "sf") %>%
-    select(admin, continent) %>%
-    mutate(wb = countrycode(admin, origin = "country.name", destination = "wb")) %>%
+  world_sf = ne_countries(type = "countries", returnclass = "sf") |>
+    select(admin, continent) |>
+    mutate(wb = countrycode(admin, origin = "country.name", destination = "wb")) |>
     select(-admin),
   
   # World Bank----------
   
   wb = read_rds(here("data", "wb.rds")),
   
-  wb_recode = wb %>%
+  wb_recode = wb |>
     mutate(country = str_replace(country, "Turkiye", "Turkey"),
-           wb = countrycode(country, origin = "country.name", destination = "wb")) %>%
-    rename(year = date) %>%
+           wb = countrycode(country, origin = "country.name", destination = "wb")) |>
+    rename(year = date) |>
     select(-iso2c, -iso3c, -country),
   
   # OxCGRT Stringency index--------
   
-  oxcgrt_recode = oxcgrt %>%
-    clean_names() %>%
+  oxcgrt_recode = oxcgrt |>
+    clean_names() |>
     mutate(date = ymd(date),
            wb = countrycode(country_name, origin = "country.name", destination = "wb"),
            national_or_subnational_saho = case_when(c6m_stay_at_home_requirements %in% 0:1 ~ 0,
                                                     c6m_stay_at_home_requirements == 2 ~ 1,
                                                     c6m_stay_at_home_requirements == 3 ~ 2),
-           national_saho = national_or_subnational_saho *  c6m_flag) %>%
+           national_saho = national_or_subnational_saho *  c6m_flag) |>
     select(wb,
            date,
            stringency_index_average,
@@ -67,22 +67,22 @@ tar_plan(
   
   # Exceptius---------
   
-  exceptius_decreter = read_excel(exceptius_file, range = "A136:B159") %>%
-    clean_names() %>%
+  exceptius_decreter = read_excel(exceptius_file, range = "A136:B159") |>
+    clean_names() |>
     rename(pays = indice_de_severite_des_mesures,
            score_decrets = x2),
   
-  exceptius_controler = read_excel(exceptius_file, range = "C136:D159") %>%
-    clean_names() %>%
+  exceptius_controler = read_excel(exceptius_file, range = "C136:D159") |>
+    clean_names() |>
     rename(pays = score_de_controle_policier,
-           score_controle = x2) %>%
+           score_controle = x2) |>
     mutate(pays = if_else(pays == "Irelande", "Irlande", pays)),
   
-  exceptius = exceptius_decreter %>%
-    left_join(exceptius_controler, by = "pays") %>%
+  exceptius = exceptius_decreter |>
+    left_join(exceptius_controler, by = "pays") |>
     mutate(pays = str_replace(pays, "Pays-bas", "Pays-Bas"),
-           pays = str_replace(pays, "Royaume Uni", "Royaume-Uni")) %>%
-    mutate(wb = countrycode(pays, origin = "country.name.fr", destination = "wb")) %>%
+           pays = str_replace(pays, "Royaume Uni", "Royaume-Uni")) |>
+    mutate(wb = countrycode(pays, origin = "country.name.fr", destination = "wb")) |>
     select(-pays),
   
   # Fariss---------
@@ -90,21 +90,21 @@ tar_plan(
   
   # Google ------
   
-  google_recode = google %>%
+  google_recode = google |>
     
     mutate(date = ymd(date),
-           wb = countrycode(country_region, origin = "country.name.en", destination = "wb")) %>%
+           wb = countrycode(country_region, origin = "country.name.en", destination = "wb")) |>
     
     filter(is.na(sub_region_1),
-           is.na(sub_region_2)) %>%
+           is.na(sub_region_2)) |>
     
     select(wb, date, ends_with("_baseline")),
   
   # Excess mortality------
   
-  excess_mortality_recode = excess_mortality %>%
+  excess_mortality_recode = excess_mortality |>
     mutate(date = ymd(date),
-           wb = countrycode(location, origin = "country.name.en", destination = "wb")) %>%
+           wb = countrycode(location, origin = "country.name.en", destination = "wb")) |>
     select(wb,
            date,
            p_scores_all_ages,
@@ -114,79 +114,79 @@ tar_plan(
   
   # Police Europe--------
   
-  police_europe = read_rds(policiers_europe_file) %>%
+  police_europe = read_rds(policiers_europe_file) |>
     pivot_wider(names_from = c(sex, isco08, unit), values_from = "values",
-                names_repair = make_clean_names) %>%
-    complete(geo, time) %>%
+                names_repair = make_clean_names) |>
+    complete(geo, time) |>
     mutate(geo = if_else(geo %in% c("Scotland", "England and Wales", "Northern Ireland (UK)"),
-                         "United Kingdom", geo)) %>%
-    group_by(geo, time) %>%
-    summarise(across(ends_with("number"), ~ sum(.x, na.rm = F)), .groups = "drop") %>%
+                         "United Kingdom", geo)) |>
+    group_by(geo, time) |>
+    summarise(across(ends_with("number"), ~ sum(.x, na.rm = F)), .groups = "drop") |>
     mutate(geo = str_replace(geo, "Türkiye", "Turkey"),
            geo = if_else(str_detect(geo, "Germany"), "Germany", geo),
            geo = if_else(str_detect(geo, "Kosovo"), "Republic of Kosovo", geo),
            year = year(time),
-           wb = countrycode(geo, origin = "country.name", destination = "wb")) %>%
+           wb = countrycode(geo, origin = "country.name", destination = "wb")) |>
     select(wb, year,
            ends_with("number") &
              contains("police")),
   
-  wb_police_europe_pcmh = wb_recode %>%
-    left_join(police_europe) %>%
-    mutate(policiers_pcm_habitants = total_police_officers_number / population * 100000) %>%
-    group_by(wb) %>%
-    mutate(policiers_pcm_habitants = mean(policiers_pcm_habitants, na.rm = TRUE)) %>%
+  wb_police_europe_pcmh = wb_recode |>
+    left_join(police_europe) |>
+    mutate(policiers_pcm_habitants = total_police_officers_number / population * 100000) |>
+    group_by(wb) |>
+    mutate(policiers_pcm_habitants = mean(policiers_pcm_habitants, na.rm = TRUE)) |>
     filter(year == 2020),
   
   # AGR-------
   
-  agr_recode = agr %>%
+  agr_recode = agr |>
     mutate(date = ymd(date),
-           wb = countrycode(country, origin = "country.name", destination = "wb")) %>%
+           wb = countrycode(country, origin = "country.name", destination = "wb")) |>
     select(-country),
   
-  agr_mode = agr_recode %>%
-    filter(between(date, ymd(20200401), ymd(20200501))) %>%
+  agr_mode = agr_recode |>
+    filter(between(date, ymd(20200401), ymd(20200501))) |>
     mutate(least_strict_day = str_remove_all(least_strict_day, "b"),
            least_strict_day = as.numeric(least_strict_day),
            least_strict_day = case_when(
              least_strict_day %in% 0:1 ~ "Sorties totalement libres",
                                         least_strict_day %in% 2:3 ~ "Sorties à justifier",
                                         least_strict_day %in% 4:5 ~ "Excercice physique limité",
-                                        least_strict_day %in% 6:7 ~ "Excercice physique interdit")) %>%
-    count(wb, least_strict_day) %>%
-    group_by(wb) %>%
-    filter(n == max(n)) %>%
-    rename(mode_least_strict_day_april = least_strict_day) %>%
+                                        least_strict_day %in% 6:7 ~ "Excercice physique interdit")) |>
+    count(wb, least_strict_day) |>
+    group_by(wb) |>
+    filter(n == max(n)) |>
+    rename(mode_least_strict_day_april = least_strict_day) |>
     select(-n),
   
   # Apple-------
   
-  apple = read_rds(apple_file) %>%
+  apple = read_rds(apple_file) |>
     mutate(date = ymd(date),
-           wb = countrycode(iso3c, origin = "iso3c", destination = "wb")) %>%
+           wb = countrycode(iso3c, origin = "iso3c", destination = "wb")) |>
     select(-iso3c),
 
   # Join--------
   
   ## Country-Date-----------
   
-  world_country_date = oxcgrt_recode %>%
-    left_join(agr) %>%
-    left_join(google) %>%
-    left_join(apple) %>%
-    left_join(excess_mortality) %>%
+  world_country_date = oxcgrt_recode |>
+    left_join(agr) |>
+    left_join(google) |>
+    left_join(apple) |>
+    left_join(excess_mortality) |>
     filter(between(date, date_beginn, date_end),
-           !is.na(wb)) %>%
-    distinct(wb, date, .keep_all = TRUE) %>%
+           !is.na(wb)) |>
+    distinct(wb, date, .keep_all = TRUE) |>
     relocate(wb, date),
   
   ## Summarise---------
   
-  world_summarise = world_sf %>%
-    st_set_geometry(NULL) %>%
-    left_join(world_country_date) %>%
-    group_by(wb, continent) %>%
+  world_summarise = world_sf |>
+    st_set_geometry(NULL) |>
+    left_join(world_country_date) |>
+    group_by(wb, continent) |>
     summarise(stringency_index_average = mean(stringency_index_average, na.rm = T), # oxcgrt
               across(ends_with("saho"), ~ sum(.x, na.rm = T)),
               across(starts_with("stayhome"), ~ sum(.x == "yes")), # agr
@@ -194,46 +194,46 @@ tar_plan(
               across(ends_with("_baseline"), ~ mean(.x, na.rm = T)), # google
               across(c("driving", "walking", "transit"), ~ mean(.x, na.rm = T)), # apple
               p_score = sum(excess_proj_all_ages, na.rm = T) / sum(projected_deaths_since_2020_all_ages, na.rm = T) * 100,
-              .groups = "drop") %>%
-    left_join(wb_police_europe_pcmh) %>%
-    left_join(agr_mode) %>%
-    left_join(exceptius) %>%
-    left_join(barcelo) %>%
+              .groups = "drop") |>
+    left_join(wb_police_europe_pcmh) |>
+    left_join(agr_mode) |>
+    left_join(exceptius) |>
+    left_join(barcelo) |>
     mutate(pays = countrycode(wb, origin = "wb", destination = "country.name.fr"),
            pays = if_else(wb == "MNE", "Monténégro", pays),
-           pays = if_else(wb == "MKD", "Macédoine du Nord", pays)) %>%
-    distinct(wb, .keep_all = T) %>%
+           pays = if_else(wb == "MKD", "Macédoine du Nord", pays)) |>
+    distinct(wb, .keep_all = T) |>
     relocate(pays, wb, continent),
   
   ## Composite score--------
   
-  world_composite_score = world_summarise %>%
+  world_composite_score = world_summarise |>
     select(pays,
            residential_percent_change_from_baseline,
            parks_percent_change_from_baseline,
            walking,
            driving,
            national_saho,
-           national_or_subnational_saho) %>%
+           national_or_subnational_saho) |>
     mutate(parks_percent_change_from_baseline = - parks_percent_change_from_baseline,
            walking = 100 - walking,
            driving = 100 - driving,
-           across(where(is.numeric), scale)) %>%
-    pivot_longer(cols = 2:7) %>%
-    group_by(pays) %>%
+           across(where(is.numeric), scale)) |>
+    pivot_longer(cols = 2:7) |>
+    group_by(pays) |>
     summarise(indice_enfermement = mean(value, na.rm = T)),
   
   # Plots---------
   
   ## Stringency Index-------
   
-  stringency_index_plot = world_summarise %>%
+  stringency_index_plot = world_summarise |>
     
     filter(continent == "Europe",
-           !is.na(stringency_index_average)) %>%
+           !is.na(stringency_index_average)) |>
     
     mutate(pays = str_wrap(pays, 5, whitespace_only = F),
-           pays = fct_reorder(pays, stringency_index_average)) %>%
+           pays = fct_reorder(pays, stringency_index_average)) |>
     
     ggplot(aes(y = stringency_index_average, x = pays)) +
     
@@ -266,7 +266,7 @@ tar_plan(
   
   ## Exceptius----------
   
-  exceptius_plot = world_summarise %>%
+  exceptius_plot = world_summarise |>
     
     ggplot(aes(x = score_decrets, y = score_controle)) +
     
@@ -296,12 +296,12 @@ tar_plan(
   
   ## Parks average------------
   
-  parks_average_plot = world_summarise %>%
+  parks_average_plot = world_summarise |>
     
     filter(continent == "Europe",
-           !is.na(parks_percent_change_from_baseline)) %>%
+           !is.na(parks_percent_change_from_baseline)) |>
     
-    mutate(pays = fct_reorder(pays, desc(parks_percent_change_from_baseline))) %>%
+    mutate(pays = fct_reorder(pays, desc(parks_percent_change_from_baseline))) |>
     
     ggplot(aes(y = pays, x = parks_percent_change_from_baseline)) +
     
@@ -320,13 +320,13 @@ tar_plan(
   
   selection_pays_europe = c("France", "Espagne", "Italie", "Pays-Bas", "Allemagne", "Danemark"),
   
-  parks_evolution_plot = world_country_date %>%
+  parks_evolution_plot = world_country_date |>
     
     mutate(pays = countrycode(wb, origin = "wb", destination = "country.name.fr"),
            pays = fct_relevel(pays, selection_pays_europe),
-           parks_roll7 = rollmean(parks_percent_change_from_baseline, k = 7, fill = NA)) %>%
+           parks_roll7 = rollmean(parks_percent_change_from_baseline, k = 7, fill = NA)) |>
     
-    filter(pays %in% selection_pays_europe) %>%
+    filter(pays %in% selection_pays_europe) |>
     
     ggplot(aes(x = date, y = parks_roll7)) +
     
@@ -342,12 +342,12 @@ tar_plan(
   
   ## Police Europe---------
   
-  world_summarise %>%
+  world_summarise |>
     
     filter(continent == "Europe",
-           !is.na(policiers_pcm_habitants)) %>%
+           !is.na(policiers_pcm_habitants)) |>
     
-    mutate(pays = fct_reorder(pays, policiers_pcm_habitants)) %>%
+    mutate(pays = fct_reorder(pays, policiers_pcm_habitants)) |>
     
     ggplot(aes(y = pays, x = policiers_pcm_habitants)) +
     
@@ -355,10 +355,10 @@ tar_plan(
   
   ## Fariss & Parks--------
   
-  fariss_parks_plot = world_summarise %>%
+  fariss_parks_plot = world_summarise |>
     
     filter(!is.na(policiers_pcm_habitants),
-           !pays %in% c("Turquie", "Luxembourg")) %>%
+           !pays %in% c("Turquie", "Luxembourg")) |>
     
     ggplot(aes(x = hr_fariss, y = parks_percent_change_from_baseline)) +
     
@@ -375,7 +375,7 @@ tar_plan(
   
   ## Police & Parks---------
   
-  police_parks_plot = world_summarise %>%
+  police_parks_plot = world_summarise |>
     
     ggplot(aes(x = policiers_pcm_habitants, y = parks_percent_change_from_baseline)) +
     
@@ -392,14 +392,14 @@ tar_plan(
   
   ## AGR & Police-----------
   
-  agr_police_plot = world_summarise %>%
+  agr_police_plot = world_summarise |>
     
     filter(!is.na(mode_least_strict_day_april),
-           !is.na(policiers_pcm_habitants)) %>%
+           !is.na(policiers_pcm_habitants)) |>
     
     mutate(mode_least_strict_day_april = str_wrap(mode_least_strict_day_april, 5),
            mode_least_strict_day_april = fct_reorder(mode_least_strict_day_april,
-                                                     policiers_pcm_habitants)) %>%
+                                                     policiers_pcm_habitants)) |>
     
     ggplot(aes(x = mode_least_strict_day_april,
                y = policiers_pcm_habitants)) +
@@ -418,8 +418,8 @@ tar_plan(
   
   ## Indice and p_score--------
   
-  indice_p_score_plot = world_summarise %>%
-    left_join(world_composite_score) %>%
+  indice_p_score_plot = world_summarise |>
+    left_join(world_composite_score) |>
     ggplot(aes(x = indice_enfermement, y = p_score)) +
     geom_point() +
     geom_text_repel(aes(label = pays), family = geom_fontfamily, size = 3) +
